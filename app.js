9bbd1f7f9a56a51e44a897401462a3167e029c9b
Cristina <cristina.colon+git@gmail.com>
2015-08-05 12:21:26 -0400

    basic functionality and nodemon

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 var express = require('express');
 var app = express();
+
+app.use(function(req,res,next){
+	console.log(req.method+ ' / ' + res.statusCode);
+	//console.log(req.url);
+	//console.log(Object.keys(req));
+	next();
+});
+
+app.get('/',function(req,res){
+	res.set('Status', 200);
+	res.send('server listening again');
+});
+
+app.get('/news',function(req,res){
+	res.set('Status', 200);
+	res.send('this is the BEST news page');
+});
+
+app.listen(3000);
