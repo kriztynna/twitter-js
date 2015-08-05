358617d25a2eb3160d294c5dc2095f882e20fc33
Cristina <cristina.colon+git@gmail.com>
2015-08-05 18:00:52 -0400

    added educational comments about setting up view engine and such

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ var path = require('path');
 var swig = require('swig');
 swig.setDefaults({ cache: false});
 
+// tells the app where to find any views for res.render
 app.engine('html', swig.renderFile);
+// tells the app to render views using the html engine
 app.set('view engine', 'html');
+// tells the app what function to use for rendering html
 app.set('views', path.normalize(__dirname+'/views'));
 
 var routes = require('./routes');
