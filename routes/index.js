ad5b6d86ca9ddd6e322176373a9cbd9e92a9fcec
Cristina <cristina.colon+git@gmail.com>
2015-08-05 17:44:12 -0400

    can post tweets now

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var path = require('path');
 
+
 var router = express.Router();
 var tweetBank = require('../tweetBank');
+
+var bodyParser = require('body-parser');
 router.use(express.static('public'));
+router.use(bodyParser.urlencoded({ extended: false }));
 
 router.use(function(req,res,next){
 	console.log(req.method+ ' / ' + res.statusCode);
@@ -16,23 +20,30 @@ router.get('/users/:name/:id',function(req,res){
 	var name = req.params.name.split(" ").map(function(a){return a[0].toUpperCase()+a.slice(1).toLowerCase();}).join(" ");
 	var id = +req.params.id;
 	var list = tweetBank.find({id: id});
-	res.render('index',{title: 'Twitter.js - Post '+id+' by '+name, tweets: list});
+	res.render('index',{title: 'Twitter.js - Post '+id+' by '+name, tweets: list, showForm: false});
 });
 
 router.get('/users/:name',function(req,res){
 	var name = req.params.name.split(" ").map(function(a){return a[0].toUpperCase()+a.slice(1).toLowerCase();}).join(" ");
 	var list = tweetBank.find({name: name});
-	res.render('index',{title: 'Twitter.js - Posts by '+name, tweets: list});
+	res.render('index',{title: 'Twitter.js - Posts by '+name, tweets: list, showForm: false});
 });
 
 router.get('/', function (req, res) {
   if(req.path==='/'){
 	  var tweets = tweetBank.list();
-	  res.render( 'index', { title: 'Twitter.js', tweets: tweets } );  	
+	  res.render( 'index', { title: 'Twitter.js', tweets: tweets, showForm: true } );  	
   }
   else {
   	res.sendFile( path.normalize(__dirname+'/../public'+req.path) );
   }
+});
+
+router.post('/submit',function(req,res){
+	var name = req.body.name;
+	var text = req.body.text;
+	tweetBank.add(name,text);
+	res.redirect('/');
 });
 
 
