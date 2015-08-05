93aaafb0aa86e638e8c6aca99eec3c4c3554539d
Kim Leung <khl291@gmail.com>
2015-08-05 14:44:49 -0400

    complete adding the swig section

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ var express = require('express');
 var swig = require('swig');
 var app = express();
 
+swig.setDefaults({ cache: false});
+
+app.engine('html', swig.renderFile)
+app.set('view engine', 'html')
+app.set('views', __dirname + '/views');
+
+
 app.use(function(req,res,next){
 	console.log(req.method+ ' / ' + res.statusCode);
 	//console.log(req.url);
@@ -11,14 +18,16 @@ app.use(function(req,res,next){
 
 app.get('/',function(req,res) {
 	var locals = {title: 'homepage', people: [{name: 'Kim'}, {name: 'Cristina'} ] };
-	console.log(__dirname);
-	swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
-	  if (err) {
-	    throw err;
-	  }
-		res.set('Status', 200);
-	  res.send(output);
-	});
+	var people = [{name: 'Full'}, {name: 'Stacker'}, {name: 'Son'}];
+	res.render( 'index', {title: 'Hall of Fame', people: people} );
+	// swig.renderFile(__dirname + '/views/index.html', locals, function (err, output) {
+	//   if (err) {
+	//     throw err;
+	//   }
+	// 	res.set('Status', 200);
+	//   res.send(output);
+	// });
+
 });
 
 app.get('/news',function(req,res){
